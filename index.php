d40a3fc9e289a4bb6aff4b1611da25493592a607
Moercell <marcel.pahnke@gmail.com>
2017-03-24 02:38:41 +0100

    piwik
    adding piwik tracking code

--- index.php
+++ index.php
@@ -8,6 +8,22 @@
     <!-- font-->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <!-- font-->
+    <!-- Piwik -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//pivik.v1b.es/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Piwik Code -->
+
 </head>
 
 <body>
