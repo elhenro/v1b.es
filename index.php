3096a6c370bc32163c5dbd620b3cff88ed113005
Henrosaurus <git@henry-schober.com>
2017-03-24 03:03:24 +0100

    modal link on subdomains-hover-folder
    removed modal button
    text for folder aligned center

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                         <h3 class="grid__item-title">our websites</h3>
                     </div>
                 </a>
-                <a href="#" class="grid__item grid__item--c2">
+                <a id="subdomains" class="grid__item grid__item--c2">
                     <div class="stack">
                         <div class="stack__deco"></div>
                         <div class="stack__deco"></div>
@@ -60,11 +60,6 @@
             </div>
         </section>
     <!-- hover Ordner end -->
-
-    <!-- Modal button -->
-        <div class="wrapper">
-            <button id="modBtn" class="modal-btn">Open Modal</button>
-        </div>
 
     <!-- Modal -->
         <div id="modal" class="modal">
