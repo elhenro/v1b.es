7a5027b21fe831fe3ea7ed6cf751a8d2434cece5
Henrosaurus <git@henry-schober.com>
2017-03-29 14:20:46 +0200

    added contactmodal
    -added impressum link, currently pointing to my website imprint
    -added $mod-yellow

--- js/modal.js
+++ js/modal.js
@@ -26,11 +26,35 @@ $(function() {
     });
 });
 
-//subdomains
+//websites
 $(function() {
     // Vars
     var modBtn  = $('#websites'),
         modal   = $('#websitemodal'),
+        close   = modal.find('.close'),
+        modContent = modal.find('.modal-content');
+    // open modal when click on open modal button
+    modBtn.on('click', function() {
+        modal.css('display', 'block');
+        modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+    });
+    // close modal when click on close button or somewhere out the modal content
+    $(document).on('click', function(e) {
+        var target = $(e.target);
+        if(target.is(modal) || target.is(close)) {
+            modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+                modal.css('display', 'none');
+                next();
+            });
+        }
+    });
+});
+
+//contact
+$(function() {
+    // Vars
+    var modBtn  = $('#contact'),
+        modal   = $('#contactmodal'),
         close   = modal.find('.close'),
         modContent = modal.find('.modal-content');
     // open modal when click on open modal button
